fdfa6c47a831d61c058353e3fb8dfd5d3a842a36
martinxapps <52510496+martinxapps@users.noreply.github.com>
2022-06-14 17:14:13 -0500

    dev.14.06.22.v1.0.0

--- src/views/resultados/page.js
+++ src/views/resultados/page.js
@@ -11,31 +11,35 @@ const DataProvider = {
         Loader.show = "";
         Loader.buttonShow = "";
 
-        var formData = new FormData($('#busquedaPaciente')[0]);
+        var tipoBusqueda = $("[name='tipoBusqueda']").val();
+
         m.request({
-            method: "POST",
-            url: "https://api.hospitalmetropolitano.org/t/v1/buscar-paciente",
-            data: formData,
-            headers: {
-                "Authorization": localStorage.accessToken,
-            },
-        })
-            .then(function (result) {
+                method: "POST",
+                url: "https://api.hospitalmetropolitano.org/t/v1/buscar-paciente",
+                data: {
+                    tipoBusqueda: tipoBusqueda,
+                    pte: DataProvider.searchField
+                },
+                headers: {
+                    "Authorization": localStorage.accessToken,
+                },
+            })
+            .then(function(result) {
                 Loader.show = "d-none";
                 Loader.buttonShow = "d-none";
 
                 DataProvider.data = result.data;
                 DataProvider.filterData();
             })
-            .catch(function (e) { })
+            .catch(function(e) {})
 
 
     },
-    loadData: function () {
+    loadData: function() {
         DataProvider.fetch();
 
     },
-    filterData: function () {
+    filterData: function() {
         var to = Math.min(DataProvider.from + DataProvider.count, DataProvider.data.length + 1);
         DataProvider.filteredData = [];
         for (var i = DataProvider.from - 1; i < to - 1; i++) {
@@ -44,38 +48,38 @@ const DataProvider = {
     },
     from: 1,
     count: 10,
-    setFrom: function (from) {
+    setFrom: function(from) {
         DataProvider.from = parseInt(from);
         DataProvider.filterData();
     },
-    setCount: function (count) {
+    setCount: function(count) {
         DataProvider.count = parseInt(count);
         DataProvider.filterData();
     },
-    nextPage: function () {
+    nextPage: function() {
         var from = DataProvider.from + DataProvider.count;
         if (from > DataProvider.data.length)
             return;
         DataProvider.from = from;
         DataProvider.filterData();
     },
-    lastPage: function () {
+    lastPage: function() {
         DataProvider.from = DataProvider.data.length - DataProvider.count + 1;
         DataProvider.filterData();
     },
-    prevPage: function () {
+    prevPage: function() {
         DataProvider.from = Math.max(1, DataProvider.from - DataProvider.count);
         DataProvider.filterData();
     },
-    firstPage: function () {
+    firstPage: function() {
         DataProvider.from = 1;
         DataProvider.filterData();
     },
-    rowBack: function () {
+    rowBack: function() {
         DataProvider.from = Math.max(1, DataProvider.from - 1);
         DataProvider.filterData();
     },
-    rowFwd: function () {
+    rowFwd: function() {
         if (DataProvider.from + DataProvider.count - 1 >= DataProvider.data.length)
             return;
         DataProvider.from += 1;
@@ -94,20 +98,20 @@ const dataView = {
             Loader.buttonShow = "d-none";
             return m('table.w-100.mt-5', [
 
-                m('tbody', DataProvider.filteredData.map(function (d) {
+                m('tbody', DataProvider.filteredData.map(function(d) {
 
 
                     return m("div.p-5.mb-3.doctrs-info-card.grad-bg--5.position-relative.type-1.radius-10", [
                         m("h4.text-white.mb-0", [
-                            m("i.icofont-ui-user"),
-                            " " + d['APELLIDOS'] + " " + d['NOMBRES']
-                        ]
+                                m("i.icofont-ui-user"),
+                                " " + d['APELLIDOS'] + " " + d['NOMBRES']
+                            ]
 
                         ),
                         m("div.text-right", [
                             m("a.btn.medim-btn.solid-btn.mt-4.text-medium.radius-pill.text-active.text-uppercase.white-btn.bg-transparent.position-relative", {
-                                href: "#!/resultados/paciente/" + d['PK_NHCL']
-                            },
+                                    href: "#!/resultados/paciente/" + d['PK_NHCL']
+                                },
                                 " Ver Paciente "
                             )
                         ])
@@ -137,24 +141,65 @@ const dataView = {
 
 const pageTool = {
     view: () => {
-        return m('div.f6.w-100.mw8.center', [
-            m("div.fl.w-50.w-20-ns.tc.pv5", [
-                m('button.f6.link.dim.br2.ph3.pv2.mb2.dib.white.bg-black', { onclick: function () { DataProvider.firstPage(); } }, '|<<'),
-                m('button.f6.link.dim.br2.ph3.pv2.mb2.dib.white.bg-black', { onclick: function () { DataProvider.prevPage(); } }, '<<'),
-                m('button.f6.link.dim.br2.ph3.pv2.mb2.dib.white.bg-black', { onclick: function () { DataProvider.rowBack(); } }, '<')
-            ]),
-            m("div.fl.w-50.w-60-ns.tc.pv5", [
-                m('span.f5.f4-m.f3-l.fw2.black-50.mt0.lh-copy', 'showing '),
-                m('input.tc.w-10.f5.f4-m.f3-l.fw2.black-50.mt0.lh-copy', { value: DataProvider.count, onchange: function (e) { DataProvider.setCount(e.target.value); } }),
-                m('span.f5.f4-m.f3-l.fw2.black-50.mt0.lh-copy', ' of ' + DataProvider.data.length + ' rows. Starting at row '),
-                m('input.tc.w-10.f5.f4-m.f3-l.fw2.black-50.mt0.lh-copy', { value: DataProvider.from, onchange: function (e) { DataProvider.setFrom(e.target.value); } })
-            ]),
-            m("div.fl.w-50.w-20-ns.tc.pv5", [
-                m('button.f6.link.dim.br2.ph3.pv2.mb2.dib.white.bg-black', { onclick: function () { DataProvider.rowFwd(); } }, '>'),
-                m('button.f6.link.dim.br2.ph3.pv2.mb2.dib.white.bg-black', { onclick: function () { DataProvider.nextPage(); } }, '>>'),
-                m('button.f6.link.dim.br2.ph3.pv2.mb2.dib.white.bg-black', { onclick: function () { DataProvider.lastPage(); } }, '>>|')
-            ])
-        ]);
+
+        if (DataProvider.data.length > 10) {
+            return m('div.d-flex.w-100.text-center.mt-5', [
+                m("div.w-50.w-20", [
+                    m("btn.fadeInDown-slide.position-relative.animated.pl-4.pr-4.lsp-0.no-border.bg-transparent.medim-btn.grad-bg--3.solid-btn.mt-0.text-medium.radius-pill.text-active.text-white.s-dp-1-2.mr-2", {
+                            type: "button",
+                            onclick: function() { DataProvider.firstPage(); }
+                        },
+                        " | << "
+                    ),
+
+                    m("btn.fadeInDown-slide.position-relative.animated.pl-4.pr-4.lsp-0.no-border.bg-transparent.medim-btn.grad-bg--3.solid-btn.mt-0.text-medium.radius-pill.text-active.text-white.s-dp-1-2.mr-2", {
+                            type: "button",
+                            onclick: function() { DataProvider.prevPage(); }
+                        },
+                        " << "
+                    ),
+                    m("btn.fadeInDown-slide.position-relative.animated.pl-4.pr-4.lsp-0.no-border.bg-transparent.medim-btn.grad-bg--3.solid-btn.mt-0.text-medium.radius-pill.text-active.text-white.s-dp-1-2.mr-2", {
+                            type: "button",
+                            onclick: function() { DataProvider.rowBack(); }
+                        },
+                        " Anterior "
+                    ),
+                ]),
+                m("div.d-none", [
+                    m('span.f5.f4-m.f3-l.fw2.black-50.mt0.lh-copy', 'showing '),
+                    m('input.tc.w-10.f5.f4-m.f3-l.fw2.black-50.mt0.lh-copy', { value: DataProvider.count, onchange: function(e) { DataProvider.setCount(e.target.value); } }),
+                    m('span.f5.f4-m.f3-l.fw2.black-50.mt0.lh-copy', ' of ' + DataProvider.data.length + ' rows. Starting at row '),
+                    m('input.tc.w-10.f5.f4-m.f3-l.fw2.black-50.mt0.lh-copy', { value: DataProvider.from, onchange: function(e) { DataProvider.setFrom(e.target.value); } })
+                ]),
+                m("div.w-50.w-20", [
+
+                    m("btn.fadeInDown-slide.position-relative.animated.pl-4.pr-4.lsp-0.no-border.bg-transparent.medim-btn.grad-bg--3.solid-btn.mt-0.text-medium.radius-pill.text-active.text-white.s-dp-1-2.mr-2", {
+                            type: "button",
+                            onclick: function() { DataProvider.rowFwd(); }
+                        },
+                        " Siguiente > "
+                    ),
+
+                    m("btn.fadeInDown-slide.position-relative.animated.pl-4.pr-4.lsp-0.no-border.bg-transparent.medim-btn.grad-bg--3.solid-btn.mt-0.text-medium.radius-pill.text-active.text-white.s-dp-1-2.mr-2", {
+                            type: "button",
+                            onclick: function() { DataProvider.nextPage(); }
+                        },
+                        " Pág. Sig. > "
+                    ),
+
+
+                    m("btn.fadeInDown-slide.position-relative.animated.pl-4.pr-4.lsp-0.no-border.bg-transparent.medim-btn.grad-bg--3.solid-btn.mt-0.text-medium.radius-pill.text-active.text-white.s-dp-1-2.mr-2", {
+                            type: "button",
+                            onclick: function() { DataProvider.lastPage(); }
+                        },
+                        " Pag. Final"
+                    ),
+
+                ])
+            ]);
+        }
+
+
     }
 };
 
@@ -202,8 +247,8 @@ const iPaciente = {
                 ]),
                 m("div.text-right", [
                     m("a.btn.fadeInDown-slide.mt-4.animated.no-border.bg-transparent.medim-btn.grad-bg--3.solid-btn.mt-0.text-medium.radius-pill.text-active.text-uppercase.text-white", {
-                        href: "#!/paciente/" + _data.attrs.HC.slice(0, -2)
-                    },
+                            href: "#!/paciente/" + _data.attrs.HC.slice(0, -2)
+                        },
                         " Ver Paciente "
                     )
                 ]),
@@ -225,6 +270,7 @@ const PageResultados = {
         }
     },
 
+
     view: () => {
 
 
@@ -243,71 +289,64 @@ const PageResultados = {
                         )
                     ),
                     m("div.row.m-mb-20",
-                        m("div.col-md-12",
-                            m("form[id='busquedaPaciente']", [
-                                m("div.d-flex.align-items-left.position-relative.justify-content-left",
-                                    [
-                                        m("div.custom-control.custom-radio.m-mb-20.ml-2.mr-2",
-                                            [
-                                                m("input.custom-control-input[type='radio'][id='cedula'][name='tipoBusqueda'][value='cc']"),
-                                                m("label.custom-control-label[for='cedula']",
-                                                    "Cédula"
-                                                )
-                                            ]
-                                        ),
-                                        m("div.custom-control.custom-radio.m-mb-20.mr-2",
-                                            [
-                                                m("input.custom-control-input[type='radio'][id='nhc'][name='tipoBusqueda'][value='nhc']"),
-                                                m("label.custom-control-label[for='nhc']",
-                                                    "Historia Clínica"
-                                                )
-                                            ]
-                                        ),
-                                        m("div.custom-control.custom-radio.m-mb-20.mr-2",
-                                            [
-                                                m("input.custom-control-input[type='radio'][id='pte'][name='tipoBusqueda'][value='pte']"),
-                                                m("label.custom-control-label[for='pte']",
-                                                    "Apellidos y Nombres"
-                                                )
-                                            ]
-                                        )
-                                    ]
-                                ),
-                                m("div.input-group.banenr-seach.bg-white.m-mt-30.mb-0", [
-                                    m("input.form-control[type='text'][name='pte'][placeholder='Buscar por Cédula, Historia Clínica, Apellidos y Nombres']", {
-                                        oninput: function (e) {
-                                            e.target.value = e.target.value.toUpperCase();
-                                            DataProvider.searchField = e.target.value;
-                                        },
-                                        value: DataProvider.searchField,
-                                    }),
-                                    m("div.input-group-append",
-                                        m("i.icofont-close.p-2.mt-1", {
-
-                                            style: { "color": "rgba(108, 117, 125, 0.4) !important", "font-size": "xx-large" },
-                                            class: (DataProvider.searchField.length !== 0) ? "" : "d-none",
-                                            onclick: () => {
-                                                DataProvider.searchField = "";
-                                                DataProvider.fetch();
-                                            },
-                                        }),
-                                        m("button.btn[type='button']", {
-                                            onclick: () => {
-                                                DataProvider.fetch();
-                                            },
-                                        },
-                                            "Buscar"
-                                        ),
-
+                        m("div.col-md-12", [
+                            m("div.d-flex.align-items-left.position-relative.justify-content-left", [
+                                m("div.custom-control.custom-radio.m-mb-20.mr-2", [
+                                    m("input.custom-control-input[type='radio'][id='pte'][name='tipoBusqueda'][value='pte']", { checked: true }),
+                                    m("label.custom-control-label[for='pte']",
+                                        "Apellidos y Nombres"
+                                    )
+                                ]),
+                                m("div.custom-control.custom-radio.m-mb-20.ml-2.mr-2", [
+                                    m("input.custom-control-input[type='radio'][id='cedula'][name='tipoBusqueda'][value='cc']"),
+                                    m("label.custom-control-label[for='cedula']",
+                                        "Cédula"
+                                    )
+                                ]),
+                                m("div.custom-control.custom-radio.m-mb-20.mr-2", [
+                                    m("input.custom-control-input[type='radio'][id='nhc'][name='tipoBusqueda'][value='nhc']"),
+                                    m("label.custom-control-label[for='nhc']",
+                                        "Historia Clínica"
                                     )
                                 ]),
 
-                            ]))
+                            ]),
+                            m("div.input-group.banenr-seach.bg-white.m-mt-30.mb-0", [
+                                m("input.form-control[type='text'][name='pte'][placeholder='Buscar por Apellidos y Nombres, Cédula, Historia Clínica']", {
+                                    oninput: function(e) {
+                                        e.target.value = e.target.value.toUpperCase();
+                                        DataProvider.searchField = e.target.value;
+                                    },
+                                    value: DataProvider.searchField,
+                                }),
+                                m("div.input-group-append",
+                                    m("i.icofont-close.p-2.mt-1", {
+
+                                        style: { "color": "rgba(108, 117, 125, 0.4) !important", "font-size": "xx-large" },
+                                        class: (DataProvider.searchField.length !== 0) ? "" : "d-none",
+                                        onclick: () => {
+                                            DataProvider.searchField = "";
+                                            DataProvider.fetch();
+                                        },
+                                    }),
+                                    m("button.btn[type='button']", {
+                                            onclick: () => {
+                                                DataProvider.fetch();
+                                            },
+                                        },
+                                        "Buscar"
+                                    ),
+
+                                )
+                            ]),
+                        ])
                     ),
                     m("div.row.m-pt-20.m-pb-60.m-mt-20", [
-                        m("div.col-12.pd-r-0.pd-l-0.pd-b-20.",
+                        m("div.col-12.pd-r-0.pd-l-0.pd-b-20.", [
                             m(dataView),
-                        )
+                            m(pageTool),
+
+                        ])
                     ])
                 )
             ),
@@ -321,6 +360,8 @@ const PageResultados = {
     },
 
 };
+
+
 
 
 export default PageResultados;
